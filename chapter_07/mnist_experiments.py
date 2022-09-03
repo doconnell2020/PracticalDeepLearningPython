4d437c5249df1855f8182d149fb438bbb3f34125
David O'Connell <oconned6@tcd.ie>
2022-09-03 09:21:31 +0100

    refactor for less repetition

--- chapter_07/mnist_experiments.py
+++ chapter_07/mnist_experiments.py
@@ -8,42 +8,65 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn import decomposition
 
+MODELS = [
+    NearestCentroid(),
+    KNeighborsClassifier(n_neighbors=3),
+    KNeighborsClassifier(n_neighbors=7),
+    GaussianNB(),
+    MultinomialNB(),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(n_estimators=5),
+    RandomForestClassifier(n_estimators=50),
+    RandomForestClassifier(n_estimators=500),
+    RandomForestClassifier(n_estimators=1000),
+    LinearSVC(C=0.01),
+    LinearSVC(C=0.1),
+    LinearSVC(C=1),
+    LinearSVC(C=10),
+]
+
+
 def run(x_train, y_train, x_test, y_test, clf):
     s = time.time()
     clf.fit(x_train, y_train)
-    e_train = time.time() - s 
+    e_train = time.time() - s
     s = time.time()
     score = clf.score(x_test, y_test)
-    e_test = time.time() - s 
+    e_test = time.time() - s
     print("score = %0.4f (time, train=%8.3f, test=%8.3f)" % (score, e_train, e_test))
 
-def train(x_train, y_train, x_test, y_test):
-    print("    Nearest centroid          : ", end='')
-    run(x_train, y_train, x_test, y_test, NearestCentroid())
-    print("    k-NN classifier (k=3)     : ", end='')
-    run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=3))
-    print("    k-NN classifier (k=7)     : ", end='')
-    run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=7))
-    print("    Naive Bayes (Gaussian)    : ", end='')
-    run(x_train, y_train, x_test, y_test, GaussianNB())
-    print("    Decision Tree             : ", end='')
-    run(x_train, y_train, x_test, y_test, DecisionTreeClassifier())
-    print("    Random Forest (trees=  5) : ", end='')
-    run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=5))
-    print("    Random Forest (trees= 50) : ", end='')
-    run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=50))
-    print("    Random Forest (trees=500) : ", end='')
-    run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=500))
-    print("    Random Forest (trees=1000): ", end='')
-    run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=1000))
-    print("    LinearSVM (C=0.01)        : ", end='')
-    run(x_train, y_train, x_test, y_test, LinearSVC(C=0.01))
-    print("    LinearSVM (C=0.1)         : ", end='')
-    run(x_train, y_train, x_test, y_test, LinearSVC(C=0.1))
-    print("    LinearSVM (C=1.0)         : ", end='')
-    run(x_train, y_train, x_test, y_test, LinearSVC(C=1.0))
-    print("    LinearSVM (C=10.0)        : ", end='')
-    run(x_train, y_train, x_test, y_test, LinearSVC(C=10.0))
+
+def train(x_train, y_train, x_test, y_test, models=MODELS):
+    for model in models:
+        print(str(model) + "       :", end="")
+        run(x_train, y_train, x_test, y_test, model)
+    # print("    Nearest centroid          : ", end="")
+    # run(x_train, y_train, x_test, y_test, NearestCentroid())
+    # print("    k-NN classifier (k=3)     : ", end="")
+    # run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=3))
+    # print("    k-NN classifier (k=7)     : ", end="")
+    # run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=7))
+    # print("    Naive Bayes (Gaussian)    : ", end="")
+    # run(x_train, y_train, x_test, y_test, GaussianNB())
+    # print("    Decision Tree             : ", end="")
+    # run(x_train, y_train, x_test, y_test, DecisionTreeClassifier())
+    # print("    Random Forest (trees=  5) : ", end="")
+    # run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=5))
+    # print("    Random Forest (trees= 50) : ", end="")
+    # run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=50))
+    # print("    Random Forest (trees=500) : ", end="")
+    # run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=500))
+    # print("    Random Forest (trees=1000): ", end="")
+    # run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=1000))
+    # print("    LinearSVM (C=0.01)        : ", end="")
+    # run(x_train, y_train, x_test, y_test, LinearSVC(C=0.01))
+    # print("    LinearSVM (C=0.1)         : ", end="")
+    # run(x_train, y_train, x_test, y_test, LinearSVC(C=0.1))
+    # print("    LinearSVM (C=1.0)         : ", end="")
+    # run(x_train, y_train, x_test, y_test, LinearSVC(C=1.0))
+    # print("    LinearSVM (C=10.0)        : ", end="")
+    # run(x_train, y_train, x_test, y_test, LinearSVC(C=10.0))
+
 
 def main():
     x_train = np.load("../data/mnist/mnist_train_vectors.npy").astype("float64")
@@ -54,12 +77,12 @@ def main():
     print("Models trained on raw [0,255] images:")
     train(x_train, y_train, x_test, y_test)
     print("Models trained on raw [0,1) images:")
-    train(x_train/256.0, y_train, x_test/256.0, y_test)
+    train(x_train / 256.0, y_train, x_test / 256.0, y_test)
 
     m = x_train.mean(axis=0)
     s = x_train.std(axis=0) + 1e-8
     x_ntrain = (x_train - m) / s
-    x_ntest  = (x_test - m) / s
+    x_ntest = (x_test - m) / s
 
     print("Models trained on normalized images:")
     train(x_ntrain, y_train, x_ntest, y_test)
@@ -68,9 +91,9 @@ def main():
     pca.fit(x_ntrain)
     x_ptrain = pca.transform(x_ntrain)
     x_ptest = pca.transform(x_ntest)
-    
+
     print("Models trained on first 15 PCA components of normalized images:")
     train(x_ptrain, y_train, x_ptest, y_test)
 
-main()
 
+main()
